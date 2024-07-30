1a06ae3e97098eb62320496f85ef3cf4a0045d3b
Colin McDonald <1colinmcdonald@gmail.com>
2024-07-29 21:15:03 -0700

    Finish project

--- script.js
+++ script.js
@@ -38,12 +38,48 @@ function addBookToLibrary(title, author, pages, read) {
     books.push(book);
     const bookDiv = document.createElement("div");
     bookDiv.classList.add("book");
+    bookDiv.setAttribute("index", books.length - 1);
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("delete");
     const deleteImage = document.createElement("img");
-    deleteImage.src = "img/close-circle.svg"
+    deleteImage.src = "img/close-circle.svg";
     deleteButton.appendChild(deleteImage);
     bookDiv.appendChild(deleteButton);
+    deleteButton.addEventListener("click", () => {
+        books.splice(deleteButton.parentElement.getAttribute("index"), 1);
+        deleteButton.parentNode.parentNode.removeChild(deleteButton.parentNode);
+    })
+    const doneReadingButton = document.createElement("button");
+    doneReadingButton.classList.add("done-reading");
+    if (read) {
+        doneReadingButton.textContent = "Done reading";
+        const doneReadingSymbol = document.createElement("img");
+        doneReadingSymbol.src = "img/check.svg";
+        doneReadingSymbol.classList.add("done-reading-symbol");
+        bookDiv.appendChild(doneReadingSymbol);
+    }
+    else
+        doneReadingButton.textContent = "Not done reading";
+    bookDiv.appendChild(doneReadingButton);
+    doneReadingButton.addEventListener("click", () => {
+        console.log("Click!");
+        const book = books[doneReadingButton.parentNode.getAttribute("index")];
+        if (book.read === false) {
+            console.log("hi!");
+            doneReadingButton.textContent = "Done reading";
+            const doneReadingSymbol = document.createElement("img");
+            doneReadingSymbol.src = "img/check.svg";
+            doneReadingSymbol.classList.add("done-reading-symbol");
+            books[doneReadingButton.parentNode.getAttribute("index")].read = true;
+            doneReadingButton.parentNode.appendChild(doneReadingSymbol);
+        } else {
+            doneReadingButton.textContent = "Not done reading"
+            const parent = doneReadingButton.parentNode;
+            parent.removeChild(parent.querySelector(".done-reading-symbol"))
+            books[parent.getAttribute("index")].read = false;
+        }
+
+    })
     const titleDiv = document.createElement("div");
     titleDiv.classList.add("title");
     titleDiv.textContent = title;
@@ -52,14 +88,8 @@ function addBookToLibrary(title, author, pages, read) {
     authorDiv.classList.add("author");
     authorDiv.textContent = author;
     bookDiv.appendChild(authorDiv);
-    bookElements.push(bookDiv);
+    bookshelf.appendChild(bookDiv);
 }
-
-function displayBooks() {
-    bookshelf.replaceChildren(...bookElements);
-}
-
-displayBooks();
 
 const addBookButton = document.querySelector('button.add-book');
 const addBookDialog = document.querySelector('dialog.add-book');
@@ -76,7 +106,6 @@ addBookDialog.addEventListener("close", () => {
         formElements = Array.from(form.elements)
         addBookToLibrary(formElements[0].value, formElements[1].value, Number(formElements[2].value), formElements[3].value)
         addBookDialog.close(true);
-        displayBooks();
         window.scrollTo(0, document.body.scrollHeight);
     } else if (addBookDialog.returnValue === "cancel") {
         addBookDialog.close(false);
